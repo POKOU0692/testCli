1a7161386f8db155805b3fbdb991bc360a291bd7
POKOU EHOUNOU FABRICE <ehounoumartial@gmail.com>
2024-11-04 13:58:01 +0000

    push du 04/11/2024

---
+++ testCli/src/main/java/com/dgtcp/testCli/TestCliApplication.java
@@ -0,0 +1,13 @@
+package com.dgtcp.testCli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCliApplication.class, args);
+	}
+
+}
